6674b255bc47f4a38def7713fdf91f3f0828a0c7
akg007 <aniruddhagawali03@gmail.com>
2023-10-08 22:36:34 +0530

    new : added splash screen

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import "package:app/theme/app_theme.dart";
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:spendwise/screens/startup.dart';
+import "package:spendwise/theme/app_theme.dart";
 
 void main() {
   runApp(const MyApp());
@@ -13,57 +16,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
-        builder: (lightDynamic, darkDynamic) => MaterialApp(
-              title: 'Flutter Demo',
-              theme: AppTheme.lightTheme(lightDynamic),
-              darkTheme: AppTheme.darkTheme(darkDynamic),
-              home: const MyHomePage(title: 'Flutter Demo Home Page'),
-            ));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      builder: (lightDynamic, darkDynamic) => MaterialApp(
+        title: 'Spend Wise',
+        theme: AppTheme.lightTheme(lightDynamic),
+        darkTheme: AppTheme.darkTheme(darkDynamic),
+        home: AnimatedSplashScreen.withScreenFunction(
+            splash: MdiIcons.wallet,
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            splashIconSize: 100,
+            screenFunction: () async {
+              await Future.delayed(const Duration(seconds: 2));
+              return StartupScreen();
+            }),
       ),
     );
   }
---
+++ app/lib/screens/startup.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class StartupScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to Finance App',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
