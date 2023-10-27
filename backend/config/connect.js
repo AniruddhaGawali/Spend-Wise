b5f1e949b9104ce44f3b82c810babe97656a09f7
Freakyab <aryanbhisikar1@gmail.com>
2023-10-27 19:26:13 +0530

    fix

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -2,7 +2,8 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 dotenv.config();
 
-const connectUrl = process.env.DEVELOPMENT ? process.env.MONGO_URI_DEV : process.env.MONGO_URI;
+
+const connectUrl = process.env.DEVELOPMENT == 'true' ? process.env.MONGO_URI_DEV : process.env.MONGO_URI;
 const client = mongoose.createConnection(connectUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
