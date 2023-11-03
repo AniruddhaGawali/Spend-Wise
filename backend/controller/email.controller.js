6b0f291cbcad344c6a7b2efd1ab720ed0be997a2
Freakyab <aryanbhisikar1@gmail.com>
2023-11-03 22:36:34 +0530

    feat: Add password reset functionality

--- backend/controller/email.controller.js
+++ backend/controller/email.controller.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const nodeMailer = require("nodemailer");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 const User = require("../model/user.model");
 const Otp = require("../model/otp.model");
@@ -12,6 +13,10 @@ const transporter = nodeMailer.createTransport({
   },
 });
 
+  /*
+    * POST /api/email/forgetPassword/:id
+  */
+
 router.post("/forgetPassword/:id", async (req, res) => {
   try {
     const email = req.params.id;
@@ -19,6 +24,11 @@ router.post("/forgetPassword/:id", async (req, res) => {
     const otp = Math.floor(Math.random() * 1000000);
 
     if (isUser) {
+      const otpExists = await Otp.findOne({ userId: isUser._id }).exec();
+      if (otpExists) {
+        return res.status(409).json({ message: "Otp already exists" });
+      }
+
       const newOtp = new Otp({
         userId: isUser._id,
         otp: otp,
@@ -43,16 +53,18 @@ router.post("/forgetPassword/:id", async (req, res) => {
       transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
           console.log(error);
+          res.status(500).json({ message: "Server error" });
         } else {
-          console.log("Email sent: " + info.response);
+          res.status(200).json({ message: "Email sent successfully" });
         }
       });
 
       setTimeout(async () => {
-        const otpTimeout = await Otp.findOneAndDelete({
-          userId: isUser._id,
-        }).exec();
-        console.log(otpTimeout);
+        try {
+          await Otp.findByIdAndDelete(newOtp._id).exec();
+        } catch (err) {
+          res.status(500).json({ message: "Server error" });
+        }
       }, 1000 * 60 * 5);
 
       res.status(200).json({ message: "OTP sent successfully" });
@@ -64,5 +76,59 @@ router.post("/forgetPassword/:id", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
+
+  /*
+    * POST /api/email/verify
+  */
+
+router.post("/verify", async (req, res) => {
+  const { otp, email } = req.body;
+  try {
+    const getGmail = await User.findOne({ email }).exec();
+    if (getGmail) {
+      const otpVerify = await Otp.findOne({ userId: getGmail._id }).exec();
+      if (otpVerify && otpVerify.otp === otp) {
+        
+        res.status(202).json({ message: "Otp verified" });
+      } else {
+        res.status(400).json({ message: "Otp not verified" });
+      }
+    } else {
+      res.status(400).json({ message: "User does not exist" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+  /*
+    * POST /api/email/resetPassword
+  */
+
+  router.post('/resetPassword', async (req, res) => {
+    try {
+      const { password,email } = req.body;
+  
+      // Check if user already exists
+      const existingUser = await User.findOne({ email });
+      if (!existingUser) {
+        return res.status(409).json({ message: 'User doesnt exists' });
+      }
+      
+      // update password
+      const newPassword = await bcrypt.hash(password, 10);
+
+      existingUser.password = newPassword;
+      await existingUser.save();
+      
+      res.status(201).json({ message: 'Reset password successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+
+
 
 module.exports = router;
