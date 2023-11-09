8622ae43bfb3c661cc3ff6c030d186c149084666
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2023-11-09 17:37:38 +0530

    update:login page added

---
+++ web/app/pages/login/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ComingSoon } from "@/app/components/ComingSoon.js/page";
+import { Navbar } from "@/app/components/Navbar/page";
+
+const LogIn = () => {
+  return (
+    <div>
+      <Navbar />
+      <ComingSoon />
+    </div>
+  );
+};
+
+export default LogIn;
