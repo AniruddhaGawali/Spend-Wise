90d214eedb15dc8595a7ac7fa34decb829790b74
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2023-11-09 17:38:07 +0530

    update:register page added

---
+++ web/app/pages/register/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ComingSoon} from '@/app/components/ComingSoon.js/page';
+import {Navbar} from '@/app/components/Navbar/page';
+
+
+const Register = () => {
+  return (
+    <div>
+        <Navbar/>
+        <ComingSoon/>
+    </div>
+  )
+}
+
+export default Register
