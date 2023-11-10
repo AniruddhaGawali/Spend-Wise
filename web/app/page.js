b2e128015c0816f587f5c587575701c1b71bf550
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2023-11-10 22:20:07 +0530

    update:home page sections added

--- web/app/page.js
+++ web/app/page.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Navbar } from "./components/Navbar/page";
 import {HeroSection} from "./components/HeroSection/page";
+import {PromotionSection} from "./components/PromotionSection/page";
+import {ScreenshotSection} from "./components/ScreenshotSection/page";
 
 export default function Home() {
   return (
     <>
      <Navbar/>
      <HeroSection/>
+      <ScreenshotSection/>
+     <PromotionSection/>
     </>
   );
 }
