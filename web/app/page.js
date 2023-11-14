48b4fceacb9da1b47fb31456196089d5290d7033
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2023-11-14 13:20:06 +0530

    refactor:landing page overflow fixed

--- web/app/page.js
+++ web/app/page.js
@@ -3,14 +3,21 @@ import { Navbar } from "./components/Navbar/page";
 import {HeroSection} from "./components/HeroSection/page";
 import {PromotionSection} from "./components/PromotionSection/page";
 import {ScreenshotSection} from "./components/ScreenshotSection/page";
+import {Footer} from "./components/Footer/page";
 
 export default function Home() {
   return (
     <>
+    <div className="overflow-y-auto overflow-x-hidden">
+
      <Navbar/>
      <HeroSection/>
       <ScreenshotSection/>
      <PromotionSection/>
+     <span className="text-center text-gray-500 text-xs">
+      <Footer />
+      </span>
+    </div>
     </>
   );
 }
