5cdab74658507b6e67b9e9a67728e6dcd5a716b0
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2023-11-15 13:59:14 +0530

    update:app icon added

--- web/app/layout.js
+++ web/app/layout.js
@@ -1,5 +1,5 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/wallet.svg" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
